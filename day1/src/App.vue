8804ccc9b8bddb49a69a69ac3f70aaa984dd1e01
Warat Wongmaneekit (Thang) <canopybanj@gmail.com>
2019-04-27 11:39:56 +0700

    Add class binding

--- day1/src/App.vue
+++ day1/src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
     <ul>
-      <li v-for="todo in todos" :key="todo.time" :id="'item-' + todo.time">
-        <span v-if="todo.completed">COMPLETED</span>
+      <li v-for="todo in todos" 
+      :key="todo.time" 
+      :id="prefix + todo.time" 
+      :class="{
+        red: (todo.completed === false),
+        green: (todo.completed === true),
+      }"
+      :style="{
+        'font-size':fontSize
+      }">
         {{todo.text | cap }}
       </li>
     </ul>
+    <form>
+      <input type="text" v-model.lazy="inputText">
+      <button type="submit">Save</button>
+    </form>
   </div>
 </template>
 
@@ -20,6 +32,9 @@ export default {
   },
   data () {
     return {
+      fontSize: '20px',
+      prefix: 'item-',
+      inputText:'',
       todos:[
         {
           text: '1st todo',
@@ -42,7 +57,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -50,5 +65,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+li {
+  list-style: none;
+}
+.red {
+  color: crimson;
+}
+.green {
+  color:darkgreen;
+}
+.big{
+  font-size: 18px;
 }
 </style>

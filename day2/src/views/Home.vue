b616b0257ae01342ce83897e27f7739833cf16ae
Warat Wongmaneekit (Thang) <canopybanj@gmail.com>
2019-04-28 10:02:53 +0700

    Add day2

---
+++ day2/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <ul>
+      <li v-for="blog in blogs" :key="blog.id">
+        {{ blog.title.rendered }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+// @ is an alias to /src
+export default {
+  name: "home",
+  data() {
+    return {
+      blogs: []
+    };
+  },
+  async mounted() {
+    let res = await axios.get("https://blog.skooldio.com/wp-json/wp/v2/posts/");
+    this.blogs = res.data;
+  }
+};
+</script>
+<style>
+li {
+  list-style: none;
+}
+</style>
